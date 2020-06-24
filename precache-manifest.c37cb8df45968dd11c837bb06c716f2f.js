91a461cd5c2c9893f57777dee3c57fde8e0f2a52
Bongani Maphiri <b.maphiri@gmail.com>
2020-06-24 23:35:52 +0200

    Updates

---
+++ precache-manifest.c37cb8df45968dd11c837bb06c716f2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a110003f9c64de58f18b8d910a89e0e6",
+    "url": "/kozmiq/index.html"
+  },
+  {
+    "revision": "7e625aa31d73bf2196c4",
+    "url": "/kozmiq/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "33322c02f789d6644d9a",
+    "url": "/kozmiq/static/css/main.408a47c5.chunk.css"
+  },
+  {
+    "revision": "7e625aa31d73bf2196c4",
+    "url": "/kozmiq/static/js/2.4686054d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kozmiq/static/js/2.4686054d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33322c02f789d6644d9a",
+    "url": "/kozmiq/static/js/main.c4e444fe.chunk.js"
+  },
+  {
+    "revision": "9ccdbf4e7db557088f0a",
+    "url": "/kozmiq/static/js/runtime-main.666970e2.js"
+  }
+]);
